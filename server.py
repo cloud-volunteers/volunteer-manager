6746b3f8fff84dbddd95aad8d603052385f4d776
L3RAT <luke.leszczynski@gmail.com>
2022-09-23 21:26:23 +0200

    add simple Jinja2 template

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
 from os import getenv
 from uvicorn import run
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 # app = FastAPI(title='Volunteer Manager', docs_url=None, redoc_url=None)
 app = FastAPI(title='Volunteer Manager')
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
 @app.get('/')
 async def root():
     return {"info": "I'm software for managing volunteers!"}
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
 if __name__ == '__main__':
     run("server:app", host='0.0.0.0', port=int(getenv('PORT')))
