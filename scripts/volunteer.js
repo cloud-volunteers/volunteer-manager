ddfdbab3b500c881e88eddd93cc710ba3f2b5842
Michał Nowicki <michal5520pl@gmail.com>
2022-10-08 21:33:45 +0200

    Modified main, volunteer HTML and JS, added volunteers, changed entrypoint to LF

--- scripts/volunteer.js
+++ scripts/volunteer.js
@@ -1,7 +1,12 @@
 function generateInside(name, placeholder, jinja){
+    var readonly = ""
+
+    if("id" == name)
+        readonly = "readonly"
+
     let label = `<label for = \"${name}\" class = \"align-middle\">${placeholder}</label>`
     let input = 
-    `<input name = \"${name}\" placeholder = \"${placeholder}\" value = \"${jinja}\" class = \"m-1 bg-slate-200 border-2 border-solid border-slate-600 shadow-md shadow-slate-300 dark:text-black\" />`
+    `<input name = \"${name}\" placeholder = \"${placeholder}\" value = \"${jinja}\" ${readonly} class = \"m-1 bg-slate-200 border-2 border-solid border-slate-600 shadow-md shadow-slate-300 dark:text-black\" />`
     let labelDiv = `<div class = \"w-[25%]\">${label}</div>`
     let inputDiv = `<div class = \"w-[25%] text-end\">${input}</div>`
     return labelDiv + inputDiv;
