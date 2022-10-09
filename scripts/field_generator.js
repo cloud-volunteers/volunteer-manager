7857c619c5ada191d8498f21439053d45375da0c
Michał Nowicki <michal5520pl@gmail.com>
2022-10-09 22:10:51 +0200

    Added multilanguage, reworked some stuff

--- scripts/field_generator.js
+++ scripts/field_generator.js
@@ -2,9 +2,12 @@ function generateInside(name, placeholder, jinja){
     var readonly = ""
     var type = "text"
     var required = ""
+    var checked = ""
 
     if(jinja == "None")
         jinja = ""
+
+    var value = `value = \"${jinja}\"`
 
     if("id" == name){
         readonly = "readonly"
@@ -15,21 +18,26 @@ function generateInside(name, placeholder, jinja){
         type = "email"
         required = "required"
     }
-    else if(("online" == name) || ("offline" == name) || ("active" == name))
+    else if(("online" == name) || ("offline" == name) || ("active" == name) || ("has_car" == name)){
         type = "checkbox"
-        
+        if("True" == jinja)
+            checked = "checked"
+
+        value = ""
+    }
+
     else if("age" == name)
         type = "number"
 
     let label = `<label for = \"${name}\" class = \"align-middle\">${placeholder}</label>`
     let input = 
-    `<input name = \"${name}\" type = \"${type}\" placeholder = \"${placeholder}\" value = \"${jinja}\" ${readonly} ${required} class = \"m-1 bg-slate-200 border-2 border-solid border-slate-600 shadow-md shadow-slate-300 dark:text-black\" />`
+    `<input name = \"${name}\" type = \"${type}\" placeholder = \"${placeholder}\" ${value} ${readonly} ${required} ${checked} class = \"m-1 bg-slate-200 border-2 border-solid border-slate-600 shadow-md shadow-slate-300 dark:text-black\" />`
     let labelDiv = `<div class = \"w-[25%]\">${label}</div>`
     let inputDiv = `<div class = \"w-[25%] text-end\">${input}</div>`
     return labelDiv + inputDiv;
 }
 
-function generateOutside(name, placeholder, jinja){
+function generateOutside(name, placeholder, jinja = ""){
     return `<div class = \"flex flex-row place-content-center m-2\">${generateInside(name, placeholder, jinja)}</div>`;
 }
 
